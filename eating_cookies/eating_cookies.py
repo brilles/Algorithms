3a30ce7795946a6583a350deed823dcd9261db94
Brian Illes <brianrilles@gmail.com>
2019-06-06 14:30:45 -0700

    Completes eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,11 +5,26 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * eating_cookies(n-1)
+# def eating_cookies(n, cache=None):
+#   if n == 0:
+#     return 1;
+#   if n <= 2: 
+#     return n
+#   else:
+#     return (n) + eating_cookies(n-1) - 1/3
+
+cache = {0: 1, 1: 1, 2: 2} # memoization
+
+def eating_cookies(n, nothing=None):
+  global cache
+
+  # val is already calculated
+  if n in cache:
+    return cache[n]
+  
+  # calc value, cache it, return 
+  cache[n] = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
